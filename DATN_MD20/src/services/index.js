daa8315167a244768a41ae20c7781bf7c2cbad7d
Hoàng Trung Hiếu <hoangtrunghieuthi@unghieuthigmail.com>
2025-07-01 22:12:06 +0700

    hieu

--- DATN_MD20/src/services/index.js
+++ DATN_MD20/src/services/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Tạo một instance
 const axiosInstance = axios.create({
-  baseURL: 'http://192.168.1.6:3000', // hoặc localhost nếu dùng trong cùng thiết bị
+  baseURL: 'http://192.168.0.103:3000', // hoặc localhost nếu dùng trong cùng thiết bị
   timeout: 30000,
 });
 
