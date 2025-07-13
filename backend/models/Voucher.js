cf6fa9aed6ca3674716da3b4ba3cb472ecbd6a2a
Nguyen Manh Dung <zck280803@gmail.com>
2025-07-13 20:24:14 +0700

    "model voucher"

--- backend/models/Voucher.js
+++ backend/models/Voucher.js
@@ -4,15 +4,15 @@ const VOUCHER_ENUM = ["percentage", "fixed"];
 
 const voucherSchema = new mongoose.Schema(
   {
-    code: { type: String, reqiured: true },
-    description: { type: String, reqiured: false, default: "" },
-    discount_type: { type: String, enum: VOUCHER_ENUM, reqiured: true },
-    discount_value: { type: String, reqiured: true },
-    min_order_amount: { type: Number, reqiured: false, default: 0 },
-    quantity: { type: Number, reqiured: true },
+    code: { type: String, required: true },
+    description: { type: String, required: false, default: "" },
+    discount_type: { type: String, enum: VOUCHER_ENUM, required: true },
+    discount_value: { type: String, required: true },
+    min_order_amount: { type: Number, required: false, default: 0 },
+    quantity: { type: Number, required: true },
     used_count: { type: Number, default: 0 },
-    date_from: { type: String, reqiured: true },
-    date_to: { type: String, reqiured: true },
+    date_from: { type: String, required: true },
+    date_to: { type: String, required: true },
     is_status: { type: Boolean, default: false },
   },
   { timestamps: true }
